5a3880997b182c5bd99f23f0f730c0993aecb14f
shuaibu222 <shuaibuabdulkadir222@gmail.com>
2024-06-04 16:36:41 +0100

    first commit

---
+++ src/test/java/com/shuaibu/SmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuaibu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
